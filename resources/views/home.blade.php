d17852bf0bbb57ab8c16ac3faba4cfc86443fe29
JorgeJmnz <jorgejmnz08@gmail.com>
2022-10-05 10:13:03 -0500

    commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,69 @@
+@extends('adminlte::page')
+
+@section('title', 'AdminHome')
+
+@section('content_header')
+    <h1>UbietySystem</h1>
+@stop
+
+@section('content')
+
+<p>Welcome to this beautiful admin panel.</p>
+    <div class="card">
+        <div class="card-header">
+            <h1>TITLE</h1>
+        </div>
+        <div class="card-body">
+        Laravel has wonderful, thorough documentation covering every aspect of the framework.
+        </div>
+    </div>
+    <div class="card">
+        <div class="card-header">
+            <h1>TITLE</h1>
+        </div>
+        <div class="card-body">
+        Laravel has wonderful, thorough documentation covering every aspect of the framework.
+        </div>
+    </div>
+    <div class="card">
+        <div class="card-header">
+            <h1>TITLE</h1>
+        </div>
+        <div class="card-body">
+        Laravel has wonderful, thorough documentation covering every aspect of the framework.
+        </div>
+    </div>
+    <div class="card">
+        <div class="card-header">
+            <h1>TITLE</h1>
+        </div>
+        <div class="card-body">
+        Laravel has wonderful, thorough documentation covering every aspect of the framework.
+        </div>
+    </div>
+    <div class="card">
+        <div class="card-header">
+            <h1>TITLE</h1>
+        </div>
+        <div class="card-body">
+        Laravel has wonderful, thorough documentation covering every aspect of the framework.
+        </div>
+    </div>
+    <div class="card">
+        <div class="card-header">
+            <h1>TITLE</h1>
+        </div>
+        <div class="card-body">
+        Laravel has wonderful, thorough documentation covering every aspect of the framework.
+        </div>
+    </div>
+
+@stop
+
+@section('css')
+    <link rel="stylesheet" href="/css/admin_custom.css">
+@stop
+
+@section('js')
+    <script> console.log('Hi!'); </script>
+@stop
